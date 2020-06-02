491d9c4ecea2f3782591cf863e45d593582f4ae4
unknown <hendrasimz92@gmail.com>
2020-06-02 23:50:03 +0700

    node --v

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express')
 const serveStatic= require('serve-static')
 const path = require('path')
 
-const app = =express()
+const app =express()
 
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8990
 app.listen(port)
 
 console.log('Listening on port : ' + port)
