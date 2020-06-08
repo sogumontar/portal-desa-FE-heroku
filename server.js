9e4d98a41d51163fe6b2dd13964b9969dd8fdd68
unknown <hendrasimz92@gmail.com>
2020-06-08 09:28:13 +0700

    all server

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const path = require('path')
 const app =express()
 
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
+app.get(/.*/,(req, res) => {
+    res.sendfile(`${__dirname}/dist/index.html`)
+});
 
 const port = process.env.PORT || 8990
 app.listen(port)
